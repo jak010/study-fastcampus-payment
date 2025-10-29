9b0d317ef30b5601b7aba54ecb2f0edb1fb767c2
jak010 <bluetoon@naver.com>
2025-10-29 17:40:24 +0900

    feat(order): Order 엔티티 추가
    - 주문 정보 관리를 위한 Order 엔티티 추가
    - 필드: id, userId, amount, requestsId, courseId, courseName, status, createdAt, updatedAt

---
+++ src/main/java/com/example/fastcampus_payment/order/Order.java
@@ -0,0 +1,46 @@
+package com.example.fastcampus_payment.order;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "temp_order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private BigDecimal amount;
+
+    private String requestsId;
+    private Long courseId;
+
+    private String courseName;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public enum Status {
+        WAIT, REQUESTED, APPROVED
+    }
+
+}
